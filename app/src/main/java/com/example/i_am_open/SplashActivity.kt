4df42f76bd0389348b7958d2e37078330819d12e
Lyubov Korovina <lukorovina@gmail.com>
2022-11-12 18:48:19 -0500

    splash screen created

--- app/src/main/java/com/example/i_am_open/SplashActivity.kt
+++ app/src/main/java/com/example/i_am_open/SplashActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
-class Splash : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -18,6 +18,6 @@ class Splash : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },2500)
+        },25000)
     }
 }
