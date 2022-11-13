7bf8718ca00009234c74d124db1a446ecdeb3e4b
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-11-13 17:37:05 -0500

    nav added and back optioin added

--- app/src/main/java/com/example/i_am_open/ProductDetailFragment.kt
+++ app/src/main/java/com/example/i_am_open/ProductDetailFragment.kt
@@ -2,11 +2,14 @@ package com.example.i_am_open
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+
 
 class ProductDetailFragment : Fragment() {
 
--- app/src/main/java/com/example/i_am_open/ProductLegalFragment.kt
+++ app/src/main/java/com/example/i_am_open/ProductLegalFragment.kt
@@ -1,11 +1,12 @@
 package com.example.i_am_open
 
-import android.app.ActionBar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+
 
 class ProductLegalFragment : Fragment() {
 
@@ -14,11 +15,16 @@ class ProductLegalFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_product_legal, container, false)
 
+
         return view
     }
+
+
+
 
 
 }
