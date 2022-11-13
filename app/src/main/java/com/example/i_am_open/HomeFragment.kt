00042d75d68f8603f1199fcdb2bb965d4b35959a
Srijan Shrestha <srijans318@gmail.com>
2022-11-13 18:13:09 -0500

    Add redirection to product screen from home screen

--- app/src/main/java/com/example/i_am_open/HomeFragment.kt
+++ app/src/main/java/com/example/i_am_open/HomeFragment.kt
@@ -18,25 +18,38 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val viewAllBtn = view.findViewById<Button>(R.id.view_all_btn)
         viewAllBtn.setOnClickListener {
-            view.findNavController().navigate(R.id.action_homeFragment2_to_companyFragment2)
+            view.findNavController().navigate(R.id.action_homeFragment_to_companyFragment)
         }
-//        val product1 = view.findViewById<ConstraintLayout>(R.id.product1)
-//        product1.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
-//
-//        val product2 = view.findViewById<ConstraintLayout>(R.id.product2)
-//        product2.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
-//
-//        val product3 = view.findViewById<ConstraintLayout>(R.id.product3)
-//        product3.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
-//
-//        val product4 = view.findViewById<ConstraintLayout>(R.id.product4)
-//        product4.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
-//
-//        val product5 = view.findViewById<ConstraintLayout>(R.id.product5)
-//        product5.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
-//
-//        val product6 = view.findViewById<ConstraintLayout>(R.id.product6)
-//        product6.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        val item1 = view.findViewById<ConstraintLayout>(R.id.item_1)
+        item1.setOnClickListener {
+            item1.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        val item2 = view.findViewById<ConstraintLayout>(R.id.item_2)
+        item2.setOnClickListener {
+            item2.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        val item3 = view.findViewById<ConstraintLayout>(R.id.item_3)
+        item3.setOnClickListener {
+            item3.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        val item4 = view.findViewById<ConstraintLayout>(R.id.item_4)
+        item4.setOnClickListener {
+            item4.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        val item5 = view.findViewById<ConstraintLayout>(R.id.item_5)
+        item5.setOnClickListener {
+            item5.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
+        val item6 = view.findViewById<ConstraintLayout>(R.id.item_6)
+        item6.setOnClickListener {
+            item6.findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+
 
         return view
     }
