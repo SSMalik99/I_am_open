1ae8e66b7ed8ab3e1307270aa04d6f8ed89afa8b
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-11-12 21:27:01 -0500

    app bar configure issue and label addedd for the screens

--- app/src/main/java/com/example/i_am_open/MainActivity.kt
+++ app/src/main/java/com/example/i_am_open/MainActivity.kt
@@ -4,6 +4,8 @@ package com.example.i_am_open
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragmentContainerView)
         bottomNavigationView.setupWithNavController(navController)
 
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.companyFragment, R.id.favouriteFragment, R.id.profileFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
 }
