7c36c0c595e0c414b4efe96a8a9b7f8e0f9f70ea
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-11-13 17:08:52 -0500

    detail fragment are changed

--- app/src/main/java/com/example/i_am_open/ProductDetailFragment.kt
+++ app/src/main/java/com/example/i_am_open/ProductDetailFragment.kt
@@ -1,18 +1,12 @@
 package com.example.i_am_open
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.findFragment
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
-
 
 class ProductDetailFragment : Fragment() {
 
@@ -24,9 +18,16 @@ class ProductDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_product_detail, container, false)
 
-//        view.findViewById<Button>(R.id.productVideoButton).setOnClickListener {
-//
+        view.findViewById<TextView>(R.id.productVideoButton).setOnClickListener {
+            view.findNavController().navigate(R.id.action_productDetailFragment_to_productVideoGuideFragment)
+        }
+
+//        view.findViewById<TextView>(R.id.productTutorialButton).setOnClickListener {
+//            view.findNavController().navigate(R.id.action_productDetailFragment_to_productTutorialFragment)
 //        }
+        view.findViewById<TextView>(R.id.productLegalButton).setOnClickListener {
+            view.findNavController().navigate(R.id.action_productDetailFragment_to_productLegalFragment)
+        }
         return view
 
     }
