265648a3457a4d07a5bf022829d494171b5843ae
Lyubov Korovina <lukorovina@gmail.com>
2022-11-12 19:12:24 -0500

    removed not-working code from the MainActivity.kt

--- app/src/main/java/com/example/i_am_open/MainActivity.kt
+++ app/src/main/java/com/example/i_am_open/MainActivity.kt
@@ -1,30 +1,12 @@
 package com.example.i_am_open
 
-import android.R
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
-import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val window: Window = activity.getWindow()
-
-        // clear FLAG_TRANSLUCENT_STATUS flag:
-        // clear FLAG_TRANSLUCENT_STATUS flag:
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-
-        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-
-        // finally change the color
-        // finally change the color
-        window.statusBarColor = ContextCompat.getColor(activity, R.color.my_statusbar_color)
     }
 }
