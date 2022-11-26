6434b2316b69a521170348820e56c00300d6c22b
Srijan Shrestha <srijans318@gmail.com>
2022-11-26 18:55:03 -0500

    Render database data in recently added products and company

--- app/src/main/java/com/example/i_am_open/CompanyAdapter.kt
+++ app/src/main/java/com/example/i_am_open/CompanyAdapter.kt
@@ -8,19 +8,21 @@ import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class CompanyAdapter(private val myContext: Context,
-                     private val arrayList: ArrayList<Company>):
-    ArrayAdapter<Company>(myContext, R.layout.company_list_item, arrayList) {
+                     private val arrayList: ArrayList<CompanyModel>):
+    ArrayAdapter<CompanyModel>(myContext, R.layout.company_list_item, arrayList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater: LayoutInflater = LayoutInflater.from(myContext)
         val view: View =  inflater.inflate(R.layout.company_list_item, null)
-        val imageView: LinearLayout = view.findViewById(R.id.companyImage)
+        val imageView: ImageView = view.findViewById(R.id.companyImage)
         val name: TextView = view.findViewById(R.id.companyName)
         val description: TextView = view.findViewById(R.id.companyDescription)
 
-        imageView.setBackgroundResource(arrayList[position].imageId)
+
+        Glide.with(myContext).load(arrayList[position].image).into(imageView)
         name.text = arrayList[position].name
         description.text = arrayList[position].description
 
