660a8baae35ffbc9ff43d6421083bf62991a472f
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-11-27 03:52:00 -0500

    favourite page is done

--- app/src/main/java/com/example/i_am_open/FavouriteFragment.kt
+++ app/src/main/java/com/example/i_am_open/FavouriteFragment.kt
@@ -1,16 +1,19 @@
 package com.example.i_am_open
 
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.i_am_open.adapters.FavouriteProductAdapter
 
 class FavouriteFragment : Fragment() {
 
-    lateinit var databaseHelper: DatabaseHelper
+    private lateinit var databaseHelper: DatabaseHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,12 +22,20 @@ class FavouriteFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_favourite, container, false)
         databaseHelper = DatabaseHelper(view.context)
-
-        val listView = view.findViewById<ListView>(R.id.favouriteProductListContainer)
-
+        val recyclerView  = view.findViewById<RecyclerView>(R.id.favouriteFragmentRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
         val products = databaseHelper.likedProducts()
-        val productAdapter = ArrayAdapter(view.context, R.layout.product_list_item, products)
-        listView.adapter = productAdapter
+
+        val emptyMessage = view.findViewById<TextView>(R.id.noneFavouriteProduct)
+        if (products.size < 1) {
+            emptyMessage.visibility = View.VISIBLE
+        }else{
+            emptyMessage.visibility = View.GONE
+            val productAdapter = FavouriteProductAdapter(view.context, products)
+            recyclerView.adapter = productAdapter
+        }
+
+
         return view
     }
 }
