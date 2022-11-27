799944126dadf5b98a0a1946f12f595410f089bc
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-11-27 18:34:39 -0500

    video page added

--- app/src/main/java/com/example/i_am_open/adapters/VideoTutorialAdapter.kt
+++ app/src/main/java/com/example/i_am_open/adapters/VideoTutorialAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.i_am_open.adapters
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.MediaController
 import android.widget.TextView
 import android.widget.VideoView
 import androidx.cardview.widget.CardView
@@ -12,6 +16,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.i_am_open.ProductDetailFragmentDirections
 import com.example.i_am_open.R
 import com.example.i_am_open.TutorialModel
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.DefaultPlayerUiController
 
 class VideoTutorialAdapter(val context: Context, val videos : ArrayList<TutorialModel>)
     : RecyclerView.Adapter<VideoTutorialHolder>() {
@@ -29,8 +38,43 @@ class VideoTutorialAdapter(val context: Context, val videos : ArrayList<Tutorial
 
     override fun onBindViewHolder(holder: VideoTutorialHolder, position: Int) {
        val video = videos[position]
-        holder.tutorialTitle.text = video.title
-//        holder.view
+        holder.tutorialTitle.text = if (video.title != "N/A") video.title else "Video Tutorial"
+//        holder.videoView.
+        holder.videoView.enableAutomaticInitialization = true
+
+        val listener: YouTubePlayerListener = object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                // We're using pre-made custom ui
+                val defaultPlayerUiController =
+                    DefaultPlayerUiController(holder.videoView, youTubePlayer)
+                defaultPlayerUiController.showFullscreenButton(true)
+
+                defaultPlayerUiController.setFullScreenButtonClickListener {
+                    holder.videoView.enterFullScreen()
+                }
+
+
+                holder.videoView.setCustomPlayerUi(defaultPlayerUiController.rootView)
+
+                val videoId = "gI4Pncpc06I"//video.description
+
+                youTubePlayer.cueVideo(videoId, 0f)
+            }
+        }
+
+//        val mc = MediaController(holder.view.context)
+//        holder.videoView.setMediaController(mc)
+//        val uri = Uri.parse(video.description)
+//        holder.videoView.setVideoURI(uri)
+//        holder.videoView.requestFocus()
+//        holder.videoView.start()
+
+//        val frameVideo = """<html><body><iframe width="420" height="315" src="${Uri.parse(video.description)}" frameborder="0" allowfullscreen></iframe></body></html>""";
+//        holder.videoView.webViewClient = WebViewClient()
+//        val webSettings = holder.videoView.settings
+//        webSettings.setJavaScriptEnabled(true);
+//        holder.videoView.loadData(frameVideo, "text/html", "utf-8");
+
 
     }
 
@@ -45,7 +89,7 @@ class VideoTutorialHolder(val view: View) :RecyclerView.ViewHolder(view) {
     var tutorialId = 0
 
     var tutorialTitle = view.findViewById<TextView>(R.id.videoTutorialTitle)!!
-    var videoView = view.findViewById<VideoView>(R.id.videoTutorialView)!!
+    var videoView = view.findViewById<YouTubePlayerView>(R.id.videoTutorialWebView)!!
 
     init {
 //        tutorialTitle.setOnClickListener{
