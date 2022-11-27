95c7597e850f91e9842bb72e7b68d30fbf15873d
Lyubov Korovina <lukorovina@gmail.com>
2022-11-26 23:13:58 -0500

    added dynamic list from product list to product details, action_companyDetailsFragment_to_productDetailFragment

---
+++ app/src/main/java/com/example/i_am_open/ProductAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.i_am_open
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+class ProductAdapter(private val myContext: Context,
+                  private val arrayList: ArrayList<ProductModel>):
+    ArrayAdapter<ProductModel>(myContext, R.layout.home_list_item, arrayList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater: LayoutInflater = LayoutInflater.from(myContext)
+        val view: View =  inflater.inflate(R.layout.home_list_item, null)
+        val imageView: ImageView = view.findViewById(R.id.productImage)
+        val name: TextView = view.findViewById(R.id.name)
+        val request: TextView = view.findViewById(R.id.request)
+        val count: TextView =  view.findViewById(R.id.count)
+        val description: TextView = view.findViewById(R.id.description)
+
+
+        Glide.with(myContext).load(arrayList[position].image).into(imageView);
+        name.text = arrayList[position].name
+        request.text = arrayList[position].upVote.toString()
+        count.text = arrayList[position].downVote.toString()
+
+        return view
+    }
+
+}
